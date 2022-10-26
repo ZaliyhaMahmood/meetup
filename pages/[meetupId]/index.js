5a87930f90eb58089896a83febf4ce7db63b9df8
Zaliyha Mahmood <mzaliyha@yahoo.com>
2022-10-26 17:51:33 +0100

    implement simple meetup app nextjs

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,77 @@
+import { MongoClient, ObjectId } from "mongodb";
+import Head from "next/head";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = (props) => {
+  return (
+    <>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Zali:zali01101996@cluster0.llyz7hw.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
+      },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  //fetch data for a single meetup
+  const client = await MongoClient.connect(
+    "mongodb+srv://Zali:zali01101996@cluster0.llyz7hw.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  console.log("selectedMeetup:", selectedMeetup);
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
+      },
+    },
+    revalidate: 1, //number of seconds it takes to regenerate page on the server
+  };
+}
+
+export default MeetupDetails;
